6872b08fb420c0eccab30c31fd152c545804c51f
T SUBRAMMANNIAM - MFCWL <23294629@mahindra.com>
2025-10-09 18:51:57 +0530

    updated Closed and Open event cases and multiuser cases

--- tests/bid.spec.ts
+++ tests/bid.spec.ts
@@ -3,7 +3,7 @@ import { LoginPage } from "../pages/LoginPage";
 import { AuctionPage } from "../pages/AuctionPage";
 import { testConfig } from "../config/testConfig";
 
-test.only("User should login and place a bid successfully", async ({ page }) => {
+test.skip("User should login and place a bid successfully", async ({ page }) => {
   const loginPage = new LoginPage(page);
   const auctionPage = new AuctionPage(page);
 
@@ -13,6 +13,36 @@ test.only("User should login and place a bid successfully", async ({ page }) => 
   await auctionPage.openFirstAuction();
   await auctionPage.placeBidOnFirstNotParticipated();
   await auctionPage.validateStatusUpdate();
-  await page.waitForTimeout(10000); // Pause for 10 seconds to observe the result
+  //await page.waitForTimeout(10000); // Pause for 10 seconds to observe the result
+  
+});
+
+test("User should login and place a bid successfully - Open Auction", async ({ page }) => {
+  console.log("Starting Open Auction Bid Test");
+  const loginPage = new LoginPage(page);
+  const auctionPage = new AuctionPage(page);
+
+  await loginPage.goto();
+  await loginPage.login(testConfig.credentials.username2, testConfig.credentials.password2);
+
+  await auctionPage.openAuction();
+  await auctionPage.placeBidOnFirstNotParticipated();
+  await auctionPage.validateStatusUpdate();
+  //await page.waitForTimeout(10000); // Pause for 10 seconds to observe the result
+  
+});
+
+test("User should login and place a bid successfully - Closed Auction", async ({ page }) => {
+  console.log("Starting Closed Auction Bid Test");
+  const loginPage = new LoginPage(page);
+  const auctionPage = new AuctionPage(page);
+
+  await loginPage.goto();
+  await loginPage.login(testConfig.credentials.username1, testConfig.credentials.password1);
+
+  await auctionPage.closedAuction();
+  await auctionPage.placeBidOnFirstNotParticipated();
+  await auctionPage.validateStatusUpdate();
+  //await page.waitForTimeout(10000); // Pause for 10 seconds to observe the result
   
 });
